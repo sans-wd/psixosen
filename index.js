1714e1e5256c7276f4e03f53caa799f96517ce44
sans-wd <alientofq1@gmail.com>
2023-03-06 23:20:11 +0400

    hi

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+(function () {
+  // Create a new Angular module
+  var app = angular.module("signUp", []);
 
+  // Get signup button, close button and modal from document
+  var signupBtn = document.getElementById("signup-btn");
+  var modal = document.getElementById("signup-modal");
+  var closeBtn = document.getElementById("close-btn");
+
+  // Create a new controller for 'app'
+  app.controller("SignUpController", function () {
+    this.firstName = "";
+    this.lastName = "";
+    this.email = "";
+    this.password = "";
+    this.logMe = function () {
+      console.log("Logged!");
+    };
+  });
+
+  // Create onclick event for signupBtn
+  signupBtn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  // Create onclick event for closeBtn
+  closeBtn.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // Create onclick event for window
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+})();
