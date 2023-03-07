becd7093d2b39aeb7d3a8a7c025a6409ed5c6d1f
sans-wd <alientofq1@gmail.com>
2023-03-08 00:00:33 +0400

    autoplayfix

---
+++ profiles/script.js
@@ -0,0 +1,24 @@
+var change = document.getElementById('change');
+
+
+document.getElementById('border').addEventListener('change', function(){
+  change.style.borderColor = this.value;
+});
+document.getElementById('border1').addEventListener('change', function(){
+    change.style.borderColor = this.value;
+  });
+
+var change1 = document.getElementsByClassName('fff')[0];
+
+document.getElementsByClassName('fff')[0].addEventListener('change', function(){
+    change1.style.backgroundColor = this.value;
+  });
+  var change2 = document.getElementsByClassName('fff')[1];
+
+document.getElementsByClassName('fff')[1].addEventListener('change', function(){
+    change2.style.backgroundColor = this.value;
+  });
+
+
+
+
