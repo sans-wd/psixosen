e5ebe1d37dcbbf83a2bd438dc980a86eaa1bb2eb
sans-wd <alientofq1@gmail.com>
2023-03-13 07:37:00 +0400

    edits

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-var change = document.getElementById('change');
+// var change = document.getElementById('change');
 
+// document.getElementById('border').addEventListener('change', function(){
+//   change.style.borderColor = this.value;
+// });
+// document.getElementById('border1').addEventListener('change', function(){
+//     change.style.borderColor = this.value;
+//   });
 
-document.getElementById('border').addEventListener('change', function(){
-  change.style.borderColor = this.value;
-});
-document.getElementById('border1').addEventListener('change', function(){
-    change.style.borderColor = this.value;
-  });
+// var change1 = document.getElementsByClassName('fff')[0];
 
-var change1 = document.getElementsByClassName('fff')[0];
+// document.getElementsByClassName('fff')[0].addEventListener('change', function(){
+//     change1.style.backgroundColor = this.value;
+//   });
+//   var change2 = document.getElementsByClassName('fff')[1];
 
-document.getElementsByClassName('fff')[0].addEventListener('change', function(){
-    change1.style.backgroundColor = this.value;
-  });
-  var change2 = document.getElementsByClassName('fff')[1];
-
-document.getElementsByClassName('fff')[1].addEventListener('change', function(){
-    change2.style.backgroundColor = this.value;
-  });
-
-
-
-
+// document.getElementsByClassName('fff')[1].addEventListener('change', function(){
+//     change2.style.backgroundColor = this.value;
+//   });
